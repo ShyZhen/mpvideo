bb08fb01ac58037c8fde5f9a81ab9b75173f0033
zhenhuaixiu <zhenhuaixiu@sznews.com>
2021-06-24 16:01:27 +0800

    video list

--- apis/index.js
+++ apis/index.js
@@ -1,141 +1,15 @@
-import http from './interface'
+import request from '../utils/request';
 
-/**
- * 将业务所有接口统一起来便于维护
- * 如果项目很大可以将 url 独立成文件，接口分成不同的模块
- * 
- */
-
-/**
- * 瀑布流页获取视频
- * @param {Number} page 页码
- * @param {Number} limit 条数
- * */
-export const getVideoInfos = (data) => {
-    let res = {
-        "code": 200,
-        "data": [
-            {
-                "vid": '60c30961a30ce80001bb437d',
-                "image":'https://m.ykimg.com/0584000060A759C92027EE090EB42DD8',
-                "title":'一不小心捡到爱',
-                "desc":'失忆霸总爱上我',  //描述|摘要，有则显示
-                "jishu":'30集全',
-                "viptype":1,  //0 不要VIP || 1 要VIP 是否显示vip角标
-            },
-            {
-                "vid": '60c30961a30ce80001bb437d',
-                "image":'https://m.ykimg.com/0584000060A759C92027EE090EB42DD8',
-                "title":'一不小心捡到爱',
-                "desc":'失忆霸总爱上我',  //描述|摘要，有则显示
-                "jishu":'30集全',
-                "viptype":1,  //0 不要VIP || 1 要VIP 是否显示vip角标
-            },
-            {
-                "vid": '60c30961a30ce80001bb437d',
-                "image":'https://m.ykimg.com/0584000060A759C92027EE090EB42DD8',
-                "title":'一不小心捡到爱',
-                "desc":'失忆霸总爱上我',  //描述|摘要，有则显示
-                "jishu":'30集全',
-                "viptype":1,  //0 不要VIP || 1 要VIP 是否显示vip角标
-            },
-            {
-                "vid": '60c30961a30ce80001bb437d',
-                "image":'https://m.ykimg.com/0584000060A759C92027EE090EB42DD8',
-                "title":'一不小心捡到爱',
-                "desc":'失忆霸总爱上我',  //描述|摘要，有则显示
-                "jishu":'30集全',
-                "viptype":1,  //0 不要VIP || 1 要VIP 是否显示vip角标
-            },
-            {
-                "vid": '60c30961a30ce80001bb437d',
-                "image":'https://m.ykimg.com/0584000060A759C92027EE090EB42DD8',
-                "title":'一不小心捡到爱',
-                "desc":'失忆霸总爱上我',  //描述|摘要，有则显示
-                "jishu":'30集全',
-                "viptype":1,  //0 不要VIP || 1 要VIP 是否显示vip角标
-            },            {
-                "vid": '60c30961a30ce80001bb437d',
-                "image":'https://m.ykimg.com/0584000060A759C92027EE090EB42DD8',
-                "title":'一不小心捡到爱',
-                "desc":'失忆霸总爱上我',  //描述|摘要，有则显示
-                "jishu":'30集全',
-                "viptype":1,  //0 不要VIP || 1 要VIP 是否显示vip角标
-            },
-            {
-                "vid": '60c30961a30ce80001bb437d',
-                "image":'https://m.ykimg.com/0584000060A759C92027EE090EB42DD8',
-                "title":'一不小心捡到爱',
-                "desc":'失忆霸总爱上我',  //描述|摘要，有则显示
-                "jishu":'30集全',
-                "viptype":1,  //0 不要VIP || 1 要VIP 是否显示vip角标
-            },
-        ],
-        "msg": "查询成功"
-    }
+function getVideoListr(type, page) {
     return new Promise((resolve, reject) => {
-        resolve(res)
-    })
-
-    return http.request({
-        url: '/http/getVideoInfos',
-        method: 'GET', 
-        data,
+        request.request('GET', 'V1/mpvideo/list/'+type+'?page='+page).then(res => {
+            resolve(res.data)
+        }).catch(e => {
+            reject(e)
+        })
     })
 }
 
-/**
- * 短视频页获取视频
- * @param {Number} vId 视频id(不传则从数据库第一个视频开始)
- * @param {Number} type 1 向上加载 2 向下加载
- * */
-export const getVideoList = (data) => {
-    return http.request({
-        url: '/http/getVideoList',
-        method: 'GET', 
-        data,
-    })
-}
-
-/**
- * 更新是否喜欢视频
- * @param {String}  uId 用户id  
- * @param {String}  vId 视频id  
- * @param {Boolean}  status 修改的状态
- * */
-export const updateVideoLike = (data) => {
-    return http.request({
-        url: '/http/updateVideoLike',
-        method: 'POST', 
-        data,
-    })
-}
-
-/**
- * 获取视频评论
- * @param {Number} page 页码
- * @param {Number} limit 条数
- * */
-export const getCommentList = (data) => {
-    return http.request({
-        url: '/http/getCommentList',
-        method: 'GET', 
-        data,
-    })
-}
-
-
-/**
- * 发表评论
- * @param {String} avatarUrl 头像URL
- * @param {String} name 名字也是用户id
- * @param {String} msg 评论的消息
- * @param {Number} vId 视频的id
- * */
-export const addComment = (data) => {
-    return http.request({
-        url: '/http/addComment',
-        method: 'POST', 
-        data,
-    })
+export {
+    getVideoListr
 }
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -16,8 +16,8 @@
 </template>
 
 <script>
+import { getVideoListr } from "@/apis/index"
 import VideoList from "../../components/videolist/video-list"
-import {getVideoInfos} from "@/apis/index"
 import LoadMore from "../../components/loadmore/load-more";
 
 export default {
@@ -67,8 +67,8 @@ export default {
         tabItem.currentPage = 1
       }
 
-      getVideoInfos(this.videoId, tabItem.currentPage).then(res => {
-        let list = res.data
+      getVideoListr(this.videoId, tabItem.currentPage).then(res => {
+        let list = res.data.data
 
         if (type === 'refresh') {
           // 刷新前清空数组
@@ -104,61 +104,8 @@ export default {
     changeType(item) {
       this.videoId = item.type
       this.loadData('add')
-      console.log(this.navList)
     },
 
-    getList(type) {
-      this.videolist = [
-        {
-          vid: '60c30961a30ce80001bb437d',
-          image:'https://m.ykimg.com/0584000060A759C92027EE090EB42DD8',
-          title:'一不小心捡到爱',
-          desc:'失忆霸总爱上我',  //描述|摘要，有则显示
-          jishu:'30集全',
-          viptype:1,  //0 不要VIP || 1 要VIP 是否显示vip角标
-        },
-        {
-          vid: '23232323',
-          image:'https://puui.qpic.cn/vcover_vt_pic/0/mzc00200y4wycre1623742797529/350',
-          title:'千古玦尘',
-          desc:'周冬雨许凯三生亘古恋',
-          jishu:'更新至14集',
-          viptype:0,
-        },
-        {
-          vid: '23232323',
-          image:'https://puui.qpic.cn/vcover_vt_pic/0/qjr4mfe8xf85j0j1622169082720/350',
-          title:'双世宠妃3',
-          desc:'爆甜！坦诚夫妇三世独宠',
-          jishu:'更新至30集',
-          viptype:1,
-        },
-        {
-          vid: '23232323',
-          image:'https://m.ykimg.com/0584000060A759C92027EE090EB42DD8',
-          title:'一不小心捡到爱',
-          desc:'失忆霸总爱上我',
-          jishu:'30集全',
-          viptype:0,
-        },
-        {
-          vid: '23232323',
-          image:'https://puui.qpic.cn/vcover_vt_pic/0/mzc00200y4wycre1623742797529/350',
-          title:'千古玦尘',
-          desc:'周冬雨许凯三生亘古恋',
-          jishu:'更新至14集',
-          viptype:1,
-        },
-        {
-          vid: '23232323',
-          image:'https://puui.qpic.cn/vcover_vt_pic/0/qjr4mfe8xf85j0j1622169082720/350',
-          title:'双世宠妃3',
-          desc:'爆甜！坦诚夫妇三世独宠',
-          jishu:'更新至30集',
-          viptype:1,
-        }
-      ]
-    },
     itemClick(item) {
       console.log(item)
       // 原生video
